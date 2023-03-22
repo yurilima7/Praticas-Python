861b51da144089fcc7cecc3c2c843bbbf5419783
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-03-22 16:27:22 -0300

    Concluindo atividade banco

--- banco/app.py
+++ banco/app.py
@@ -2,3 +2,11 @@ from conta import Conta
 
 c = Conta("Jimmy", 20, "Masculino", "Nubank")
 c.abrirConta(2000, 0) 
+c.depositar(500.50)
+c.sacar(560)
+print(c.__str__())
+c.depositar(2.55)
+c.saldo()
+c.sacar(233.50)
+c.saldo()
+c.fecharConta()
--- banco/conta.py
+++ banco/conta.py
@@ -41,7 +41,7 @@ class Conta(Cliente, AcoesBanco):
         self.__aberta = aberta  
         
     def abrirConta(self, numero, digito):
-        if(self.isAberta()):
+        if self.isAberta():
             print("Conta já está aberta!")
         else:
             self.setAberta(True)
@@ -51,22 +51,41 @@ class Conta(Cliente, AcoesBanco):
             self.setSaldo(0.0)
 
     def fecharConta(self):
-        if(self.isAberta() == False):
+        if self.isAberta() == False:
             print("Impossivel encerrar conta que não esta aberta!")
         else:
-            if(self.getSaldo() == 0):
+            if self.getSaldo() == 0:
                 self.setAberta(False)
                 print("Conta encerrada com sucesso!")
-            elif(self.getSaldo() < 0):
+            elif self.getSaldo() < 0:
                 print("Impossivel encerrar conta, pois cliente está em debito com o banco!")
             else:
-                print("Impossivel encerrar conta, pois ha saldo na mesma!")
+                print("Impossivel encerrar conta, pois há saldo na mesma!")
 
     def sacar(self, valor):
-        pass
+        if self.isAberta() == False:
+            print("Impossível sacar pois a conta não existe!")
+        else:
+            if self.getSaldo() > 0 and self.getSaldo() >= valor:
+                self.setSaldo(self.getSaldo() - valor)
+                print('Saque realizado com sucesso!')
+            elif self.getSaldo() > 0 and self.getSaldo() <= valor:
+                print('Impossível sacar, saldo insuficiente!')
+            else:
+                print("Impossível sacar!")
     
     def depositar(self, valor):
-        pass
+        if self.isAberta() == False:
+            print("Impossivel depositar, pois conta não existe!")
+        else:
+            self.setSaldo(self.getSaldo() + valor)
+            print("Deposito feito com sucesso!")
 
     def saldo(self):
-        pass
+        if self.isAberta():
+            print("Saldo da conta = R$ {:.2f}" .format(self.getSaldo()))
+        else:
+            print("Erro conta inexistente!")
+
+    def __str__(self):
+        return f"\nSOBRE A CONTA:\nnúmero da conta com digito: {self.getConta()}-{self.getDigito()}\nnome do banco: {self.getNomeBanco()}\n" + super().__str__()
